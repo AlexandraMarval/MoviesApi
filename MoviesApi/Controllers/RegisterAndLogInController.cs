465d67e5381b2ecd5c556e9d61bc3f298037859b
Alexandra marval <alexandraI.Marvala@gmail.com>
2023-09-26 11:49:52 +0200

    Add DTOS AuthenticationResponse and UserCredentials
    In the program Configure Swagger for authentication

--- MoviesApi/Controllers/RegisterAndLogInController.cs
+++ MoviesApi/Controllers/RegisterAndLogInController.cs
@@ -1,6 +1,61 @@
-﻿namespace MoviesApi.Controllers
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MoviesApi.DTOs;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MoviesApi.Controllers
 {
-    public class RegisterAndLogInController
+    [ApiController]
+    [Route("Api/RegisterAndLogIn")]
+    public class RegisterAndLogInController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+
+        public RegisterAndLogInController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<AuthenticationResponse>> Register(UserCredentials userCredentials)
+        {
+            var user = new IdentityUser { UserName = userCredentials.Email, Email = userCredentials.Email };
+            var response = await userManager.CreateAsync(user, userCredentials.Password);
+
+            if (response.Succeeded)
+            {
+                return BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest(response.Errors);
+            }
+        }
+        private AuthenticationResponse BuildToken(UserCredentials userCredentials)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim("Email", userCredentials.Email),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["keyjwt"]));
+            var Creds =  new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddYears(1);
+
+            var securityToken = new JwtSecurityToken(issuer: null, audience: null, claims: claims, expires: expiration, signingCredentials: Creds);
+
+            return new AuthenticationResponse()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
+                Expiration = expiration,
+            };
+        }
     }
 }
--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using MoviesApi;
 using MoviesApi.Service;
+using System.Security.Cryptography.Xml;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,11 +16,39 @@ builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.Re
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{}
+        }
+
+    });
+
+});
+
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddTransient<IRentalMovieService, RentalMovieService>();
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
 //builder.Services.AddTransient<NotFoundException>();
 
 builder.Services.AddResponseCaching();
@@ -38,3 +69,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
